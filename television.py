20e7c8ca9e308a8bdc093ca5803428c9d547f295
CoopBuff09 <cooperbuffington@unomaha.edu>
2024-11-22 17:48:58 -0600

    Changes to television

---
+++ television.py
@@ -0,0 +1,16 @@
+
+
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME  
+        self.__channel = Television.MIN_CHANNEL  
+
+    def power(self):
+        pass
